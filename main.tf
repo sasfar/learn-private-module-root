f33e863fa616bf941a577cff2c83fcbabac7d332
Asfar Shah <sasfar@users.noreply.github.com>
2020-09-24 10:06:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  #source  = "app.terraform.io/SAASGlobal/terraform/s3-webapp/aws"
-  source = "https://github.com/sasfar/learn-private-module-root/blob/master/main.tf"
+  source  = "app.terraform.io/SAASGlobal/s3-webapp/aws"
+  #source = "https://github.com/sasfar/learn-private-module-root/blob/master/main.tf"
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "0.13.2"
+  version = "1.0.0"
 }
 
-terraform {
-  required_version = "0.13.2"
-}
+#terraform {
+#  required_version = "0.13.2"
+#}
